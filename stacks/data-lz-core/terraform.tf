cd0c720e0beeefae5577c97b24827dc068f0966a
coder-prathamesh23 <157107915+coder-prathamesh23@users.noreply.github.com>
2026-03-04 19:18:23 +0530

    After Network
    Removed hub provider alias configuration from terraform.tf.

--- stacks/data-lz-core/terraform.tf
+++ stacks/data-lz-core/terraform.tf
@@ -14,11 +14,3 @@ provider "azurerm" {
   features {}
 }
 
-# Hub provider alias:
-# - If hub is same subscription: omit hub_subscription_id and this will behave the same as default provider.
-# - If hub is different subscription: set hub_subscription_id in tfvars.
-provider "azurerm" {
-  alias           = "hub"
-  features        {}
-  subscription_id = var.hub_subscription_id != "" ? var.hub_subscription_id : null
-}
